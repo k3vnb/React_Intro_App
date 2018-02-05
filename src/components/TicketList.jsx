c442bae14120ecb3e6fb4cb3ad8c1fbfef52379a
Kevin Boyle and Luke Bertram <luke@>
2018-02-05 10:19:36 -0800

    set up react app based on lesson instructions

---
+++ src/components/TicketList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Ticket from "./Ticket";
+
+function TicketList(){
+  return (
+    <Ticket
+      location="Mars-Base 7"
+      names="El-one 7 nine & Bill"
+      issue="Firebase literally on fire. Also out of Sweet'n'Low"/>
+  );
+}
+
+export default TicketList;
